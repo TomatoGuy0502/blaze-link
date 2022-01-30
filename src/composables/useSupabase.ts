a21cc9be8dfe2be8d6fdf50ca3d1c736f48a9c81
TomatoGuy0502 <H34066131@gs.ncku.edu.tw>
2022-01-30 15:36:36 +0800

    feat: project setup

---
+++ src/composables/useSupabase.ts
@@ -0,0 +1,10 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey)
+
+export const useSupabase = () => {
+  return { supabase }
+}
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import router from './router'
+import App from './App.vue'
+import './index.css'
+
+const app = createApp(App)
+
+app.use(createPinia()).use(router).mount('#app')
