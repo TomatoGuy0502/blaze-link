a1fb3d48fcfbb010fee98604f37d3cd773033d8b
TomatoGuy0502 <H34066131@gs.ncku.edu.tw>
2022-04-24 09:09:00 +0800

    feat: 新增 dark mode 按鈕

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,6 +2,7 @@
   <div class="relative flex items-center gap-x-8 rounded-bl-2xl bg-gray-100 py-4 px-6">
     <h1 class="text-3xl font-bold text-gray-700">{{ $route.meta.headingChinese }} {{ $route.name }}</h1>
     <div class="ml-auto flex gap-x-4">
+      <ThemeToggle />
       <div class="relative">
         <IconBell class="h-12 w-12 cursor-pointer p-2 text-gray-500" />
         <span class="absolute top-2 right-2 h-3 w-3 rounded-full bg-red-500/90"></span>
@@ -55,6 +56,7 @@ import IconBell from '~icons/heroicons-outline/bell/'
 import IconMockAvatar from '@/assets/mock-avatar.svg'
 import { useConfirmModal } from '../composables/useConfirmModal'
 import { useAuthStore } from '../store/auth'
+import ThemeToggle from './ThemeToggle.vue'
 
 const { openModal } = useConfirmModal()
 const authStore = useAuthStore()
---
+++ src/components/ThemeToggle.vue
@@ -0,0 +1,45 @@
+<template>
+  <button
+    class="relative inline-flex items-center self-center rounded-full bg-slate-700 py-1.5 px-2 text-slate-400 transition-colors duration-300 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-offset-white"
+    :class="
+      isDark
+        ? 'bg-slate-700 text-cyan-400 focus-visible:ring-slate-500'
+        : 'bg-cyan-500 text-cyan-200 focus-visible:ring-cyan-600'
+    "
+    type="button"
+    @click="() => toggleDark()"
+  >
+    <IconSun
+      class="h-6 w-6 transform transition-transform"
+      :class="isDark ? 'scale-100 duration-300' : 'scale-0 duration-500'"
+    />
+    <IconMoon
+      class="ml-3.5 h-6 w-6 transform transition-transform"
+      :class="isDark ? 'scale-0 duration-500' : 'scale-100 duration-300'"
+    />
+    <span
+      class="absolute top-0.5 left-0.5 flex h-8 w-8 transform items-center justify-center rounded-full bg-white transition duration-500"
+      :class="isDark ? 'translate-x-[2.625rem]' : ''"
+    >
+      <IconSun
+        class="h-6 w-6 flex-none transform text-cyan-500 transition duration-500"
+        :class="isDark ? 'scale-0 opacity-0' : 'scale-100 opacity-100'"
+      />
+      <IconMoon
+        class="-ml-6 h-6 w-6 flex-none transform text-slate-700 transition duration-500"
+        :class="isDark ? 'scale-100 opacity-100' : 'scale-0 opacity-0'"
+      />
+    </span>
+  </button>
+</template>
+
+<script setup lang="ts">
+import { useDark, useToggle } from '@vueuse/core'
+import IconSun from '~icons/akar-icons/sun-fill/'
+import IconMoon from '~icons/akar-icons/moon-fill/'
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
+</script>
+
+<style scoped></style>
